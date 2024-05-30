46e0224aa7114cc1bbad3be92f475a5e5766b0a0
Megija Grīnberga <grinbergamegija@gmail.com>
2024-05-30 12:42:52 +0000

    LxLink: new component (#870)

---
+++ src/components/Link.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+import { computed } from 'vue';
+
+const props = defineProps({
+  href: { type: [String, Object], default: null },
+});
+
+const isHrefString = computed(() => typeof props.href === 'string');
+</script>
+
+<template>
+  <div class="lx-link-wrapper">
+    <template v-if="href">
+      <template v-if="isHrefString">
+        <a :href="href" rel="noopener noreferrer"> <slot /></a>
+      </template>
+      <template v-else>
+        <RouterLink :to="href"> <slot /></RouterLink>
+      </template>
+    </template>
+    <template v-else>
+      <slot />
+    </template>
+  </div>
+</template>
