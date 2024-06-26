a0a2cba2bec44957011061f8b5a77fc6d2247dee
MeatyRenegade <barkancev.alexander@gmail.com>
2024-06-26 09:25:01 +0000

    Icons on load animation fix (#994)

--- src/components/icons/material/Check.vue
+++ src/components/icons/material/Check.vue
@@ -1,3 +1,5 @@
 <template>
-  <path transform="translate(0,1056)" d="M382-240 154-468l57-57 171 171 367-367 57 57-424 424Z" />
+  <g>
+    <path transform="translate(0,1056)" d="M382-240 154-468l57-57 171 171 367-367 57 57-424 424Z" />
+  </g>
 </template>
--- src/components/icons/material/FirstPage.vue
+++ src/components/icons/material/FirstPage.vue
@@ -1,6 +1,8 @@
 <template>
-  <path
-    transform="translate(128,960), scale(0.75)"
-    d="M240-240v-480h72v480h-72Zm480-24L384-480l336-216v432Z"
-  />
+  <g>
+    <path
+      transform="translate(128,960), scale(0.75)"
+      d="M240-240v-480h72v480h-72Zm480-24L384-480l336-216v432Z"
+    />
+  </g>
 </template>
