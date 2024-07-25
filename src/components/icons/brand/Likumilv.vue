45c75164347d64b9006c980477738cd1f301d6a2
WNTR Tech <wntr@wntr.tech>
2024-07-25 11:18:47 +0000

    New logos (#1134)
    Author: sergejs expeon@expeon.lv

--- src/components/icons/brand/Likumilv.vue
+++ src/components/icons/brand/Likumilv.vue
@@ -1,13 +1,13 @@
 <template>
   <g>
-    <rect x="2" y="5.6" class="st0" width="6.6" height="6.6" />
-    <path class="st0" d="M13.7,7.6v2.6h-2.6V7.6H13.7 M15.7,5.6H9.1v6.6h6.6V5.6L15.7,5.6z" />
-    <path class="st0" d="M28,7.6v2.6h-2.6V7.6H28 M30,5.6h-6.6v6.6H30V5.6L30,5.6z" />
-    <rect x="2" y="12.7" class="st0" width="6.6" height="6.6" />
-    <path class="st0" d="M13.7,14.7v2.6h-2.6v-2.6H13.7 M15.7,12.7H9.1v6.6h6.6V12.7L15.7,12.7z" />
-    <path class="st0" d="M28,14.7v2.6h-2.6v-2.6H28 M30,12.7h-6.6v6.6H30V12.7L30,12.7z" />
-    <rect x="2" y="19.8" class="st0" width="6.6" height="6.6" />
-    <rect x="9.1" y="19.8" class="st0" width="6.6" height="6.6" />
-    <path class="st0" d="M20.9,21.8v2.6h-2.6v-2.6H20.9 M22.9,19.8h-6.6v6.6h6.6V19.8L22.9,19.8z" />
+    <rect x="2" y="5.6" width="6.6" height="6.6" />
+    <path d="M15.7,5.6H9.1v6.6h6.6L15.7,5.6L15.7,5.6z" />
+    <path d="M30,5.6h-6.6v6.6H30V5.6L30,5.6z" />
+    <rect x="2" y="12.7" width="6.6" height="6.6" />
+    <path d="M15.7,12.7H9.1v6.6h6.6L15.7,12.7L15.7,12.7z" />
+    <path d="M30,12.7h-6.6v6.6H30V12.7L30,12.7z" />
+    <rect x="2" y="19.8" width="6.6" height="6.6" />
+    <rect x="9.1" y="19.8" width="6.6" height="6.6" />
+    <path d="M22.9,19.8h-6.6v6.6h6.6V19.8L22.9,19.8z" />
   </g>
 </template>
