fb87a4649e7bab47cc632225e6053c92a8c3dc75
Megija Grīnberga <grinbergamegija@gmail.com>
2024-08-01 11:27:29 +0000

    LxList: groupDefinitions id fix (#1161)

--- src/components/list/List.vue
+++ src/components/list/List.vue
@@ -1152,7 +1152,7 @@ function moveDraggableItem(direction, element, groupType) {
           @select-all="selectSection(group)"
         >
           <ul
-            :id="id"
+            :id="`${id}-${prepareCode(group.id)}`"
             class="lx-list"
             :class="[{ 'lx-list-3': listType === '3' }, { 'lx-list-2': listType === '2' }]"
             v-if="
@@ -1372,7 +1372,7 @@ function moveDraggableItem(direction, element, groupType) {
         :label="group.name"
       >
         <div
-          :id="id"
+          :id="`${id}-${prepareCode(group.id)}`"
           class="lx-list"
           :class="[{ 'lx-list-3': listType === '3' }, { 'lx-list-2': listType === '2' }]"
         >
