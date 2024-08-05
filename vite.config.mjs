a5144c6962daed3557a6eeda2657ddc09e2a8b25
MeatyRenegade <barkancev.alexander@gmail.com>
2024-08-05 08:35:10 +0000

    CSS styles minify and bundling (#914)

--- vite.config.mjs
+++ vite.config.mjs
@@ -2,14 +2,32 @@ import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 import path from 'path';
 import dts from 'vite-plugin-dts';
+import fs from 'fs';
+
+function generateInput() {
+  const input = {};
+  const stylesDir = path.resolve(__dirname, 'src/styles');
+  const files = fs.readdirSync(stylesDir);
+
+  const excludedFiles = ['lx-bt-digives.css', 'lx-bt-eikis.css'];
+
+  files.forEach((file) => {
+    if (!excludedFiles.includes(file)) {
+      const { name } = path.parse(file);
+      input[name] = path.resolve(stylesDir, file);
+    }
+  });
+
+  return input;
+}
 
 // https://vitejs.dev/config/
 /** @type {import('vite').UserConfig} */
-export default defineConfig({
+const vueConfig = defineConfig({
   base: './',
   resolve: {
     alias: {
-      '@': path.resolve('src'),
+      '@': path.resolve(__dirname, 'src'),
     },
   },
   build: {
@@ -44,4 +62,64 @@ export default defineConfig({
     globals: true,
     environment: 'happy-dom',
   },
+});
+
+/** @type {import('vite').UserConfig} */
+const cssConfig = defineConfig({
+  base: './',
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, 'src'),
+    },
+  },
+  build: {
+    outDir: 'dist/styles',
+    rollupOptions: {
+      input: generateInput(),
+      output: {
+        inlineDynamicImports: false,
+        assetFileNames: '[name][extname]',
+      },
+    },
+    cssCodeSplit: true,
+    copyPublicDir: false,
+  },
+});
+
+/** @type {import('vite').UserConfig} */
+const bundlesConfig = defineConfig({
+  base: './',
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, 'src'),
+    },
+  },
+  build: {
+    outDir: 'dist/bundles',
+    rollupOptions: {
+      input: {
+        'lx-bt-digives': path.resolve(__dirname, 'src/styles/lx-bt-digives.css'),
+        'lx-bt-eikis': path.resolve(__dirname, 'src/styles/lx-bt-eikis.css'),
+      },
+      output: {
+        inlineDynamicImports: false,
+        assetFileNames: '[name][extname]',
+      },
+    },
+    cssCodeSplit: true,
+    copyPublicDir: false,
+  },
+});
+
+export default defineConfig(({ mode }) => {
+  if (mode === 'lib') {
+    return vueConfig;
+  }
+  if (mode === 'css') {
+    return cssConfig;
+  }
+  if (mode === 'bundles') {
+    return bundlesConfig;
+  }
+  throw new Error('Unknown mode!');
 });
