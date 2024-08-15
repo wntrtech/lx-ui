8e8c5edc79d93f5cd9c3a3f27822382c98d28439
WNTR Tech <wntr@wntr.tech>
2024-08-15 09:15:41 +0000

    Address format (#1207)
    Author: Andrejs andrejs.prucevs@zzdats.lv

--- src/utils/formatUtils.js
+++ src/utils/formatUtils.js
@@ -67,3 +67,98 @@ export function formatFieldName(name) {
 export function objectClone(value) {
   return JSON.parse(JSON.stringify(value));
 }
+
+/** @typedef Address
+ * @property {string} [country]
+ * @property {string} [county]
+ * @property {string} [city]
+ * @property {string} [postalCode]
+ * @property {string} [streetName]
+ * @property {string} [buildingNumber]
+ * @property {string} [buildingNumberNumeric]
+ * @property {string} [buildingNumberSuffix]
+ * @property {string} [unitId]
+ * @property {string} [streetAddressLine]
+ * @property {string} [atvkCode]
+ * @property {string} [atvkName1]
+ * @property {string} [atvkName2]
+ * @property {string} [atvkName3]
+ * @property {string} [atvkName4]
+ */
+
+export function formatAddress(/** @type {Address} */ address, includeAtvk = false) {
+  if (!address) {
+    return EMPTY_VALUE;
+  }
+
+  let streetLine;
+  if (address.streetName) {
+    streetLine = address.streetName;
+
+    // Usually regular (numeric) building number
+    // is stored in a buildingNumberNumeric field.
+    if (address.buildingNumberNumeric) {
+      streetLine += ` ${address.buildingNumberNumeric}`;
+    }
+
+    // But in some rare cases regular (numeric) building number
+    // is stored in a buildingNumber field.
+    if (address.buildingNumber) {
+      streetLine += ` ${address.buildingNumber}`;
+    }
+  } else if (address.buildingNumber) {
+    streetLine = address.buildingNumber;
+  }
+
+  if (streetLine && address.buildingNumberSuffix) {
+    streetLine += `/${address.buildingNumberSuffix}`;
+  }
+
+  if (streetLine && address.unitId) {
+    streetLine += `-${address.unitId}`;
+  }
+
+  const addressParts = [];
+  if (streetLine) {
+    addressParts.push(streetLine);
+
+    if (address.city) {
+      addressParts.push(address.city);
+    } else {
+      if (address.atvkName4) {
+        addressParts.push(address.atvkName4);
+      }
+      if (address.atvkName3) {
+        addressParts.push(address.atvkName3);
+      }
+    }
+
+    if (address.postalCode) {
+      let { postalCode } = address;
+      if (!postalCode.startsWith('LV') && postalCode.length === 4) {
+        // Case when postal code includes only numbers.
+        postalCode = `LV-${postalCode}`;
+      } else if (postalCode.indexOf('-') === -1) {
+        // Case when postal code isn't formatted with '-'.
+        postalCode = `${postalCode.slice(0, 2)}-${postalCode.slice(2)}`;
+      }
+      addressParts.push(postalCode);
+    }
+  } else {
+    addressParts.push(address.streetAddressLine);
+  }
+
+  addressParts.push(address.country);
+
+  let addressString = addressParts.filter((line) => !!line).join(', ');
+
+  if (!addressString) {
+    return EMPTY_VALUE;
+  }
+
+  if (includeAtvk && address.atvkCode) {
+    addressString += ` (${address.atvkCode})`;
+  }
+
+  return addressString;
+}
