ff425900e035d531557a161a229bfa4ec678dbbc
Niklavs-M <niklavs.mezeckis@gmail.com>
2025-04-10 12:23:53 +0000

    LxViewLayout new component (!2071)

---
+++ src/components/ViewLayout.vue
@@ -0,0 +1,15 @@
+<script setup>
+defineProps({
+  kind: { type: String, default: 'default' }, // default | form | list
+  layout: { type: String, default: 'default' }, // default
+});
+</script>
+<template>
+  <div class="lx-view-layout">
+    <div v-if="kind !== 'form' && $slots?.filters" class="filters-container">
+      <slot name="filters" />
+    </div>
+    <div v-if="$slots?.default" class="default-container"><slot /></div>
+    <div v-if="$slots?.aside" class="aside-container"><slot name="aside" /></div>
+  </div>
+</template>
