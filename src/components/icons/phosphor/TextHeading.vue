9682e9de55c32fbda5c77ccd32f3fdbd1f28e996
ernestimos <ernests.zeiris@gmail.com>
2025-07-10 11:39:32 +0000

    Add new phosphor icons (!2173)

---
+++ src/components/icons/phosphor/TextHeading.vue
@@ -0,0 +1,9 @@
+<template>
+  <g>
+    <path d="M200,56V200H56V56Z" class="lx-shade"> </path>
+    <path
+      d="M208,56V200a8,8,0,0,1-16,0V136H64v64a8,8,0,0,1-16,0V56a8,8,0,0,1,16,0v64H192V56a8,8,0,0,1,16,0Z"
+    >
+    </path>
+  </g>
+</template>
