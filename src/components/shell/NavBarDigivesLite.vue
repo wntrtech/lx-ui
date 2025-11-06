ab6bc84b160bff89cfbf4d9dea5b08270d89fcdb
Agada-nonet <Seliverstovsdaniils@gmail.com>
2025-11-06 13:19:20 +0000

    LxShell: Small digives-lite user menu fix (!2429)

--- src/components/shell/NavBarDigivesLite.vue
+++ src/components/shell/NavBarDigivesLite.vue
@@ -664,31 +664,34 @@ onClickOutside(navPanel, toggleNavBar);
           </div>
 
           <template #panel>
-            <div class="lx-region user-menu-context">
-              <div class="lx-avatar-display lx-avatar-display-xl">
-                <LxIcon value="doctor" />
+            <div class="user-menu-panel">
+              <div class="lx-region user-menu-context">
+                <div class="lx-avatar-display lx-avatar-display-xl">
+                  <LxIcon value="doctor" />
+                </div>
+                <div class="lx-data">{{ fullName }}</div>
+                <div class="lx-description" v-if="userInfo?.description">
+                  {{ userInfo?.description }}
+                </div>
+                <div class="lx-description" v-if="userInfo?.role">{{ userInfo?.role }}</div>
+                <div class="lx-description" v-if="userInfo?.institution">
+                  {{ userInfo?.institution }}
+                </div>
               </div>
-              <div class="lx-data">{{ fullName }}</div>
-              <div class="lx-description" v-if="userInfo?.description">
-                {{ userInfo?.description }}
-              </div>
-              <div class="lx-description" v-if="userInfo?.role">{{ userInfo?.role }}</div>
-              <div class="lx-description" v-if="userInfo?.institution">
-                {{ userInfo?.institution }}
-              </div>
+
+              <ul class="lx-group" role="group">
+                <li v-for="item in navItemsUserMenu" :key="item.label">
+                  <LxButton
+                    kind="ghost"
+                    :label="item.label"
+                    :href="item.to"
+                    :icon="item.icon"
+                    :disabled="headerNavDisable"
+                    @click="emits('navClick', item?.id)"
+                  />
+                </li>
+              </ul>
             </div>
-            <ul class="lx-group" role="group">
-              <li v-for="item in navItemsUserMenu" :key="item.label">
-                <LxButton
-                  kind="ghost"
-                  :label="item.label"
-                  :href="item.to"
-                  :icon="item.icon"
-                  :disabled="headerNavDisable"
-                  @click="emits('navClick', item?.id)"
-                />
-              </li>
-            </ul>
           </template>
         </LxDropDownMenu>
       </div>
